70786133ac78e6bb663891c45562263f00f82b92
rohit2775 <rg7558674@gmail.com>
2026-03-13 21:41:53 +0530

    begining with scratch

---
+++ javascriptFundamentals/script.js
@@ -0,0 +1,10 @@
+//  alert("hello") 
+
+//starting backend from scratch , and beginning with the fundamwntals of javascript
+// arrays and objects
+
+var arr =[1,3,4,5,6,7,"hello", "rohit"]
+
+arr.forEach((val)=>{
+console.log(val);
+})
