788415cee70d8142ea02c46ee83e80bcc4cce72f
rohit2775 <rg7558674@gmail.com>
2026-03-13 22:28:09 +0530

    done with 1/4

--- javascriptFundamentals/script.js
+++ javascriptFundamentals/script.js
@@ -1,6 +1,6 @@
 //  alert("hello") 
 
-//starting backend from scratch , and beginning with the fundamwntals of javascript
+//starting backend from scratch , and beginning with the fundamentals of javascript
 // arrays and objects
 
 var arr =[1,3,4,5,6,7,"hello", "rohit"]
@@ -8,3 +8,20 @@ var arr =[1,3,4,5,6,7,"hello", "rohit"]
 arr.forEach((val)=>{
 console.log(val);
 })
+
+var array = [1,2,3,4,5,6,7,8,9]
+ var  mapping = array.map((val)=>{
+    return val*2;
+
+})
+console.log(mapping);
+
+var arr1 =[1,2,3,4,5,6,7,8.,9]
+
+var ans = arr1.filter((val)=>{
+  if(val % 2 ==0){return true}
+  else return false;
+})
+console.log(ans );
+
+// also  array.find . and array.indexof()
